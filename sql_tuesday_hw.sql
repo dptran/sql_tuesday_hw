06f574b3f3cad34679a2d0e1abb3aa6ce5c22468
dptran <darren-tran@hotmail.com>
2021-11-17 08:37:38 -0800

    latest commit

--- sql_tuesday_hw.sql
+++ sql_tuesday_hw.sql
@@ -41,10 +41,11 @@ from customer
 where last_name like '%es' and store_id = 1
 
 --9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers  with ids between 380 and 430? (use group by and having > 250) 
-select amount, count(amount)
-from payment
+select distinct amount, count(rental_id) 
+from payment 
+where customer_id between 380 and 430
 group by amount
-order by count desc
+having count(rental_id) > 250
 
 --10. Within the film table, how many rating categories are there? And what rating has the most  movies total?
 select rating, count(rating)
